d2245ad48150dc75f760552ae21be182fa336a17
Fullfifax <fullfifax1245@gmail.com>
2023-07-02 13:35:21 +0000

    add UserManager & inject it to UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -23,14 +23,14 @@ class UserController extends AbstractController
     }
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserRepository $userRepository, UserManager $userManager): Response
+    public function new(Request $request, UserManager $userManager): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
-            $userRepository->save($user, true);
+            $userManager->add($user, true);
 
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -50,13 +50,13 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    public function edit(Request $request, User $user, UserManager $userManager): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
+            $userManager->edit($user, true);
 
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -68,10 +68,10 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    public function delete(Request $request, User $user, UserManager $userManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $userRepository->remove($user, true);
+            $userManager->delete($user, true);
         }
 
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Manager;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 
 class UserManager {
@@ -11,5 +12,30 @@ class UserManager {
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+    }
+
+    public function add(User $entity, bool $flush = false): void
+    {
+        $this->entityManager->persist($entity);
+
+        if ($flush) {
+            $this->entityManager->flush();
+        }
+    }
+
+    public function edit(User $entity, bool $flush = false): void
+    {
+        if ($flush) {
+            $this->entityManager->flush();
+        }
+    }
+
+    public function delete(User $entity, bool $flush = false): void
+    {
+        $this->entityManager->remove($entity);
+
+        if ($flush) {
+            $this->entityManager->flush();
+        }
     }
 }
